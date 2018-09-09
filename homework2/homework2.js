bf3fe8c711ad83ba35954ba320b42e653c74a74a
Aram Astsatryan <aramastsatryan2000@gmail.com>
2018-09-09 15:29:28 +0400

    first commit

---
+++ homework2/homework2.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 3;
+console.log(a + b);
+console.log(a - b);
+const c = "AUA student"
+if(c.length = 11){
+	console.log("It's OK");
+}else{
+	console.log("It's not so OK");
+}
